1a931006f431349fd0887c370e6aa3dd09aae376
Pankaj5859 <pankajjhawork@gamil.com>
2025-03-06 21:35:35 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 const express=require("express")
 const cors=require("cors")
+require("dotenv").config()
+const connectDB=require("./config/db")
 
 const app=express()
 
 app.use(express.json())
 app.use(cors())
-require("dotenv").config()
-
 const PORT= process.env.port
 
-
-app.listen(PORT,()=>{
-     console.log(`Server connected to port : ${PORT}`)
+connectDB()
+.then(()=>{
+     console.log("DB connected!")
+     app.listen(PORT,()=>{
+          console.log(`Server connected to port : ${PORT}`)
+     })
 })
+.catch(()=>{
+console.log("DB not connected!")
+})
+
+
+
